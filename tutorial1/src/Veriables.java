f70ec60a9c1e797f9f7d759e915c4f78e336acc1
hariharank <khari89@gmail.com>
2014-09-13 17:42:26 +0530

    added: variables java program

---
+++ tutorial1/src/Veriables.java
@@ -0,0 +1,33 @@
+
+
+public class Veriables {
+
+    public static void main(String[] args) {
+        // Working with Variables
+        String msg="Variables are Awesome!";
+        System.out.println(msg);
+        
+        //Reassigning Variables
+        String my_msg="Variables are fantastic!";
+        my_msg="Variables are super!";
+        System.out.println(my_msg);
+        
+        char c='h';
+        System.out.println(c);
+                
+        
+        //Number Variables
+        int a=2014;
+        double pi=3.14;
+        System.out.println(a);
+        System.out.println(pi);
+        
+        //Variables within other variables
+        int five=5;
+        int ten=10;
+        int sum = five + ten;
+        System.out.println(sum);
+        
+    }
+
+}
