963782896d0848962f7ba49d57565745fb7b03c1
hariharank <khari89@gmail.com>
2014-09-13 17:19:37 +0530

    added DataTypes java program

---
+++ tutorial1/src/DataTypes.java
@@ -0,0 +1,43 @@
+public class DataTypes {
+public static void main(String args[]){
+    //Working with Strings
+    System.out.println("String letters 10 11 12 $%<!");
+    
+    //Working with Integers
+    System.out.println(50);
+    System.out.println(30000);
+    System.out.println(5+5-2*4/2);
+    
+    //Working with Doubles
+    System.out.println(1.2);
+    System.out.println(1.75);
+    System.out.println(1.75+2.5);
+    System.out.println(7.5E3);
+    
+    //Working with primitive data types
+    byte a=10;
+    System.out.println(a);
+    
+    short s=10000;
+    System.out.println(s);
+    
+    int b=3234326;
+    System.out.println(b);
+    
+    long c=1232542345234596667l;
+    System.out.println(c);
+    
+    float d=234.5f;
+    System.out.println(d);
+    
+    double e=234.5;
+    System.out.println(e);
+    
+    boolean one=false;
+    System.out.println(one);
+    
+    char f='A';
+    System.out.println(f);
+    
+}
+}
